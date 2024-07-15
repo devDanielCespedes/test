73988dbfdf80a8e9cf486eec8120199ca794da84
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2024-07-15 13:47:09 -0300

    vivareal e lopes funcionando

--- lopesImoveis/lopesImoveis.js
+++ lopesImoveis/lopesImoveis.js
@@ -7,33 +7,33 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const pageUrl =
-  "https://www.vivareal.com.br/imovel/casa-2-quartos-sao-mateus-zona-leste-sao-paulo-com-garagem-78m2-aluguel-RS1250-id-2727844548/";
+  "https://www.lopes.com.br/imovel/REO496459/venda-apartamento-3-quartos-sao-paulo-jardim-america?listFrom=sugerido&listPosition=1";
 
 const imovelId = getImovelIdByRegex({
-  regex: /id-(\d+)\//,
+  regex: /https:\/\/www\.lopes\.com\.br\/imovel\/([A-Z]{3}\d+)/,
   stringToApplyRegex: pageUrl,
 });
 
-const vivaRealImgSubDirs = [
-  "340w",
-  "768w",
-  "1080w",
-  "200x200",
-  "614x297",
-  "870x707",
-];
+const lopesImoveisImgSubDirs = [imovelId];
 
 const { imgsDir, imovelDir } = createDirectories({
   imovelId,
-  imgSubDirs: vivaRealImgSubDirs,
+  imgSubDirs: lopesImoveisImgSubDirs,
   dirName: __dirname,
 });
+
+const modifyHtmlForLopes = (htmlContent, imovelDir) => {
+  const logoPath = path.join(imovelDir, "img/REO835156/default_logo.svg");
+  return htmlContent.replace("/assets/svgs/default_logo.svg", logoPath);
+};
+
 capturePage({
   pageUrl,
-  imgSubDirs: vivaRealImgSubDirs,
+  imgSubDirs: lopesImoveisImgSubDirs,
   imgsDir,
   imovelDir,
-  waitForSelector: ".carousel-photos--item",
+  waitForSelector: "img",
+  modifyHtmlForLopes,
 })
   .then((outputPath) => {
     console.log(`Página capturada e salva como '${outputPath}'`);
